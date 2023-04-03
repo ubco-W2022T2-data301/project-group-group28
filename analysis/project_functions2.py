23f5c7f85f4e909d4bf399afc13adb50494193b6
aadilshaji <97542911+aadilshaji@users.noreply.github.com>
2023-04-02 18:02:12 -0700

    Delete project_functions2.py

--- analysis/project_functions2.py
+++
@@ -1,4 +0,0 @@
-def load_and_process(path):
-    import pandas as pd
-    df = pd.read_csv(path).drop(["Forest area", "Individuals using the Internet", "People practicing open defecation", "Electric power consumption", "Year", "GDP per capita", "Military expenditure", "Beer consumption per capita"], axis = 1).rename(columns = {"CO2 emissions": "CO2"})
-    return df
