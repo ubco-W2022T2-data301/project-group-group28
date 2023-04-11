c5437002d9290f476972fc91c8dbc9bdf1e7caab
kjassani <99561514+kjassani@users.noreply.github.com>
2023-04-11 15:50:43 -0700

    Update Project_Functions1.py

--- analysis/code/Project_Functions1.py
+++ analysis/code/Project_Functions1.py
@@ -11,7 +11,7 @@ def load_and_wrangle_data(raw_data_path):
     """
     df = (load_data(raw_data_path)
           .drop(['Year', 'Population', 'Military expenditure', 'People practicing open defecation'], axis=1)
-          .assign(high_gdp=lambda x: x['GDP per capita'] > 17246.112996855245)
+          .assign(high_gdp=lambda x: x['GDP per capita'] > 17246.112996855245) #the value of mean GDP per capita
           .apply(lambda x: x.replace(0, np.nan) if x.name != 'Beer consumption per capita' else x, axis=0)
           .dropna()
           .rename(columns={'high_gdp': 'High GDP per capita'})
