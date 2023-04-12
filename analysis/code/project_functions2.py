c8474d8da854e7ede8918714d901b69a09f03132
aadilshaji <97542911+aadilshaji@users.noreply.github.com>
2023-04-11 17:03:32 -0700

    Add files via upload

--- analysis/code/project_functions2.py
+++ analysis/code/project_functions2.py
@@ -3,10 +3,22 @@ import numpy as np
 def load_data(path):
     return pd.read_csv(path, sep=';')
 
+
+
+# def load_and_process(path):
+#     df = (load_data(path)
+#           .drop(['Population', 'Forest area', 'Military expenditure', 'Beer consumption per capita'], axis=1)
+#           .apply(lambda x: x.replace(0, np.nan) if x.name != 'Beer consumption per capita' else x, axis=0)
+#           .apply(lambda y: y > x["People using at least basic drinking water services"].mean()))
+#           .dropna()
+#          )
+#     return df
+
 def load_and_process(path):
     df = (load_data(path)
           .drop(['Population', 'Forest area', 'Military expenditure', 'Beer consumption per capita'], axis=1)
           .apply(lambda x: x.replace(0, np.nan) if x.name != 'Beer consumption per capita' else x, axis=0)
+          .assign(above_average_water_services=lambda x: x["People using at least basic drinking water services"].apply(lambda y: y > x["People using at least basic drinking water services"].mean()))
           .dropna()
          )
     return df
